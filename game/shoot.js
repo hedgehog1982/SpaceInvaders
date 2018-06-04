fbf93b438cd61b1703e6b5857affcd1ef165512b
wayne <hedgehog1982@gmail.com>
2018-06-04 17:26:28 +0100

    fix weird bug

--- game/shoot.js
+++ game/shoot.js
@@ -1,11 +1,8 @@
 setInterval(() => {
     console.log('checking bullets');
     bulletArray.forEach((bullet, index) => {
-
-
-
         //move bullet
-        //update the bullets
+
         bullet.moveSprite(bullet.x, bullet.y - 10);
         console.log(bullet.y);
         if (bullet.y <= 110) {
@@ -15,17 +12,22 @@ setInterval(() => {
             bulletArray.splice(index, 1);
             
         }
-        enemyArray.forEach((enemy,enemyIndex) => {
-            if (bullet.x <= enemy.x + enemy.width - 8  && bullet.y <= enemy.y + enemy.height - 8 ) {
-                if (bullet.y)
-                console.log("a hit")
-                bullet.removeSprite();
-                bulletArray.splice(index, 1);
-                enemy.removeSprite();
-                enemyArray.splice(enemyIndex, 1);
-            }
+
+        //update the bullets
+            enemyArray.forEach((enemy,enemyIndex) => {
+                if ( enemy.x <= bullet.x  && bullet.x <= enemy.x + enemy.width  ) {
+                if (bullet.y <= enemy.y){  // needs to check
+                        console.log("a hit")
+                        bullet.removeSprite();
+                        bulletArray.splice(index, 1);
+                        enemy.removeSprite();
+                        enemyArray.splice(enemyIndex, 1)
+                }
+                }
+
+                })
 
         })
         
-    });
-}, 50);
+    }
+, 50);
