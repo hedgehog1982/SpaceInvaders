476dab77a82001c9af7ada34c23ea84a61168da4
Justas Daugintis <>
2018-06-04 15:44:16 +0100

    gulp fille update

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var gulp = require('gulp'),
     useref = require('gulp-useref'),
     uglify = require('gulp-uglify'),
     gulpIf = require('gulp-if'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
     cssnano = require('gulp-cssnano'),
     concat = require("gulp-concat");
 
@@ -12,22 +14,40 @@ var gulp = require('gulp'),
         .pipe(gulp.dest('dist'));
 });*/
 
-gulp.task('sass', function(){
-    return gulp.src('game/*.scss')
-    .pipe(sass())
-    .pipe(concat('one.css'))
-    .pipe(gulp.dest('game/css'))
-    .pipe(browserSync.reload({
-        stream: true
-    }))
+// gulp.task('sass', function(){
+//     return gulp.src('game/*.scss')
+//     .pipe(sass())
+//     .pipe(concat('one.css'))
+//     .pipe(gulp.dest('game/css'))
+  
+// });
+gulp.task('browserSync', function() {
+    browserSync.init({
+      server: {
+        baseDir: 'game'
+      },
+    })
+  })
+
+gulp.task('concat', function() {
+    return gulp.src('css/**/*.css')
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('./'));
+})
+
+gulp.task('watch', ['browserSync','concat', 'useref'], function() {
+
+    //gulp.watch('game/index.html', browserSync.reload);
+    gulp.watch('game/**/*.*', browserSync.reload);
+    //gulp.watch('game/js/script.js', browserSync.reload);
 });
 
 gulp.task('useref', function() {
     return gulp.src('game/*.html')
     .pipe(useref())
-    .pipe(gulpIf('game/*.js', uglify()))
-    .pipe(gulpIf('game/*.css', cssnano()))
-    .pipe(gulp.dest('ex'))
+    .pipe(gulpIf('**/*.js', uglify()))
+    .pipe(gulpIf('**/*.css', cssnano()))
+    .pipe(gulp.dest('game/js'))
 });
 
 
