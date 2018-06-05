bf5c3d0c325eb727b586b6fd70ba33c8896c1459
Justas Daugintis <>
2018-06-05 13:51:04 +0100

    some changes

--- game/drawLines.js
+++ game/drawLines.js
@@ -1,15 +1,14 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
+var score = 0;
 ctx.font = '30px Arial';
-ctx.fillText('SCORE : THE SCORE GOES HERE', 10, 50);
+ctx.fillText('SCORE : ',10, 50);
+ctx.fillText(score,200, 50)
 
 ctx.beginPath();
 ctx.moveTo(0, 100);
 ctx.lineTo(700, 100);
 ctx.stroke();
 
-ctx.beginPath();
-ctx.moveTo(0, 600);
-ctx.lineTo(700, 600);
-ctx.stroke();
+
 //gameplay within 50px to 650px;
