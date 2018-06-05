4a4ecb0358e55415a05abb5b1d6a25537018f1c1
wayne <hedgehog1982@gmail.com>
2018-06-05 16:23:36 +0100

    for loops needed

--- game/enemies.js
+++ game/enemies.js
@@ -1,35 +1,40 @@
-let enemyArray = []
+let enemyArray = [];
 
-let enemyShip = new moveableStaticSprite (
+
+////for loop needed here to push lots to an array
+enemyArray.push(new moveableStaticSprite(
     30,
     100,
     30,
     30,
     '../images/cat.png',
     'myCanvas'
- )
- enemyShip.currentDirection = "right";
+));
+enemyArray[0].currentDirection = 'right';
 
 setInterval(() => {
+
+    // for eachloop if you call enemy ship as the first variable it will stop you needing to rename
+
     var dx = 10;
     var dy = 10;
-    
-    if (enemyShip.currentDirection === "left") {
+    let enemyShip = enemyArray[0]
+    if (enemyShip.currentDirection === 'left') {
         enemyShip.moveSprite(enemyShip.x - dx, enemyShip.y);
-    if (enemyShip.x <= 0) {
-       enemyShip.moveSprite(enemyShip.x + dx, enemyShip.y + dy);
-        enemyShip.currentDirection = "right";
+        if (enemyShip.x <= 0) {
+            enemyShip.moveSprite(enemyShip.x + dx, enemyShip.y + dy);
+            enemyShip.currentDirection = 'right';
+        }
     }
-    }
-    if (enemyShip.currentDirection === "right") {
+    if (enemyShip.currentDirection === 'right') {
         enemyShip.moveSprite(enemyShip.x + dx, enemyShip.y);
- 
-    if (enemyShip.x >= 600) {
-       enemyShip.moveSprite(enemyShip.x - dx, enemyShip.y + dy);
-       enemyShip.currentDirection = "left";
-    }
+
+        if (enemyShip.x >= 600) {
+            enemyShip.moveSprite(enemyShip.x - dx, enemyShip.y + dy);
+            enemyShip.currentDirection = 'left';
+        }
     }
     if (enemyShip.y >= 500) {
         // alert("Game Over!");
     }
- }, 50);
+}, 50);
