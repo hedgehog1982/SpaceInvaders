f3d71fe0cc332d729766c49a65c01cf73be17331
Justas Daugintis <>
2018-06-06 11:34:16 +0100

    shoot

--- game/shoot.js
+++ game/shoot.js
@@ -2,7 +2,7 @@ setInterval(() => {
     //console.log('checking bullets');
     bulletArray.forEach((bullet, index) => {
         //move bullet
-       // console.log(bullet)
+        // console.log(bullet)
         bullet.moveSprite(bullet.x, bullet.y - 5);
         console.log(bullet.y);
         if (bullet.y <= 105) {
@@ -10,79 +10,53 @@ setInterval(() => {
             console.log(bullet.y);
             bullet.removeSprite();
             bulletArray.splice(index, 1);
-            
         }
 
         //update the bullets
-            enemyArray.forEach((enemy,enemyIndex) => {
-                if ( enemy.x <= bullet.x + bullet.width  && bullet.x <= enemy.x + enemy.width  ) {
-                if (bullet.y <= enemy.y + enemy.height - 5){  // needs to check
-                        console.log("a hit")
-                        var audio = new Audio('../sounds/explosion1.mp3');
-                        audio.play();
-                        bullet.removeSprite();
-                        bulletArray.splice(index, 1);
-                        enemy.removeSprite();
-                        enemyArray.splice(enemyIndex, 1)
-                        hit = true;
-                        scored();
+        enemyArray.forEach((enemy, enemyIndex) => {
+            if (
+                enemy.x <= bullet.x + bullet.width &&
+                bullet.x <= enemy.x + enemy.width
+            ) {
+                if (bullet.y <= enemy.y + enemy.height - 5) {
+                    // needs to check
+                    console.log('a hit');
+                    var audio = new Audio('../sounds/explosion1.mp3');
+                    audio.play();
+                    bullet.removeSprite();
+                    bulletArray.splice(index, 1);
+                    enemy.removeSprite();
+                    enemyArray.splice(enemyIndex, 1);
+                    hit = true;
+                    scored();
                 }
+            }
+        });
+
+        buildingArray.forEach((building, buildingIndex) => {
+            if (
+                building.x <= bullet.x + bullet.width &&
+                bullet.x <= building.x + building.width
+            ) {
+                if (bullet.y <= building.y + building.height) {
+                    // needs to check
+                    console.log('a hit');
+                    var audio = new Audio('../sounds/explosion1.mp3');
+                    audio.play();
+                    bullet.removeSprite();
+                    bulletArray.splice(index, 1);
+                    building.hitSquare();
+                    if (building.destroyLimit <= 0) {
+                        buildingArray.splice(buildingIndex, 1);
+                    }
                 }
+            }
+        });
+    });
 
-<<<<<<< HEAD
-                })
-
-                buildingArray.forEach((building,buildingIndex) => {
-                    if (building.x <= bullet.x + bullet.width  && bullet.x <= building.x + building.width  ) {
-                    if (bullet.y <= building.y + building.height ){  // needs to check
-                            console.log("a hit")
-                            var audio = new Audio('../sounds/explosion1.mp3');
-                            audio.play();
-                            bullet.removeSprite();
-                            bulletArray.splice(index, 1);
-                            building.hitSquare();
-                            if (building.destroyLimit <= 0){
-                                buildingArray.splice(buildingIndex, 1)
-                            }
-
-                    }
-                    }
-    
-                    })
-
-        })
-        lowestSprites()
-        hit = false; 
-    }
-, 20);
-
-setInterval(() => {
-    
-enemyBullets.forEach((bullet, bulletIndex) => {
-    //move bullet
-   // console.log(bullet)
-    enemyBullets.moveSprite(bullet.x, bullet.y + 5);
-    console.log(bullet.y);
-    if (enemyBullets.y <= 700) {
-        //remove bullet
-        console.log(bullet.y);
-        enemyBullets.removeSprite();
-        enemyBullets.splice(bulletIndex, 1);
-        
-    }
-})
+    hit = false;
 }, 20);
 
-const lowestSprites = () => {
-    let uniqueX = []
-    let uniqueY = []
-    //get all x values 
-    enemyArray.forEach(enemy => {
-        if (uniqueX.indexOf(enemy.x) === -1){ //if not contained in index push new values
-            uniqueX.push(enemy.x)
-            uniqueY.push(enemy.y)
-        } else {  //x exists and we have its array 
-=======
 setInterval(() => {
     // make a blank array
     console.log(enemyBullets.length);
@@ -117,7 +91,7 @@ setInterval(() => {
 
         });
     });
-
+    enemyBullets = enemyBullets.filter((enemyBullets) => {return enemyBullets !== ""})
 }, 100);
 
 const lowestSprites = () => {
@@ -131,26 +105,37 @@ const lowestSprites = () => {
             uniqueY.push(enemy.y);
         } else {
             //x exists and we have its array
->>>>>>> 68f8b538b85cfb3c5e46ceef9cebc9fe306b3324
             //find current X in X index and finding corresponding Y
-            let indexOfXValue = uniqueX.indexOf(enemy.x)
-            if (uniqueY[indexOfXValue] < enemy.y) { //corresponding Y value is more than replace
-                uniqueY[indexOfXValue] = enemy.y
+            let indexOfXValue = uniqueX.indexOf(enemy.x);
+            if (uniqueY[indexOfXValue] < enemy.y) {
+                //corresponding Y value is more than replace
+                uniqueY[indexOfXValue] = enemy.y;
             }
         }
+    });
+    console.log('start of values');
+    console.log(uniqueX);
+    console.log(uniqueY);
+    var audio = new Audio('../sounds/laser1.mp3');
+    let random = Math.floor(Math.random() * uniqueX.length);
+    audio.play();
+    enemyBullets.push(
+        new moveableStaticSprite(
+            uniqueX[random] + 10,
+            uniqueY[random] + 40,
+            25,
+            25,
+            '../images/taco.png',
+            'myCanvas'
+        )
+    );
+};
 
-<<<<<<< HEAD
-    })
-    console.log("start of values")
-    console.log(uniqueX)
-    console.log(uniqueY)
-
-}
-=======
 
 setInterval(() => {
     lowestSprites();
 }, 1500);
 
->>>>>>> 68f8b538b85cfb3c5e46ceef9cebc9fe306b3324
+
+
 
